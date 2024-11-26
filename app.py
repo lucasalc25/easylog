7bd7ab22e3c32a95b0a4bd91fce285e566ba54c0
Lucas <lucas.alc25@gmail.com>
2024-11-26 17:01:37 -0400

    v3.0

--- app.py
+++ app.py
@@ -1,83 +1,115 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-import mensagem_texto
-import mensagem_imagem
+from tkinter import filedialog, messagebox, Menu
 
-# Função para selecionar o arquivo de contatos
-def selecionar_arquivo():
-    caminho_arquivo = filedialog.askopenfilename(
-        title="Selecione um arquivo de contatos",
-        filetypes=[("Arquivos de texto", "*.txt")]
-    )
-    if caminho_arquivo:
-        entrada_arquivo.config(state="normal")  # Ativa a entrada para inserir o texto
-        entrada_arquivo.delete(0, tk.END)      # Limpa o campo atual
-        entrada_arquivo.insert(0, caminho_arquivo)  # Insere o caminho do arquivo
-        entrada_arquivo.config(state="readonly")  # Desativa a entrada novamente
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sistema de Mensagens")
+        self.root.geometry("450x450")
 
-def executar(event, combobox):
-    # Obter o valor selecionado
-    opcao = combobox.get()
+        # Centralizar a janela
+        self.center_window()
 
-    # Verificar a opção e executar a função correspondente
-    if opcao == "Mensagem de Texto":
-        mensagem_texto()
-    elif opcao == "Mensagem de Imagem":
-        mensagem_imagem()
+        # Criar menu
+        self.menu = Menu(root)
+        self.root.config(menu=self.menu)
 
-# Cria a janela principal
-janela = tk.Tk()
-janela.title("Pedagobot")
-janela.iconbitmap("./icone.ico")
-janela.configure(bg='#dcdde1')
+        # Adicionar abas ao menu
+        self.menu.add_command(label="Mensagens")
+        self.menu.add_command(label="Históricos", state=tk.DISABLED)  # Desabilitado
+        self.menu.add_command(label="Coletar Dados", state=tk.DISABLED)  # Desabilitado
 
-# Define o tamanho da janela e centraliza
-largura_tela = janela.winfo_screenwidth()
-altura_tela = janela.winfo_screenheight()
-largura_janela = int(largura_tela * 0.4)
-altura_janela = int(altura_tela * 0.55)
-pos_x = (largura_tela - largura_janela) // 2
-pos_y = (altura_tela - altura_janela) // 2
-janela.geometry(f"{largura_janela}x{altura_janela}+{pos_x}+{pos_y}")
+        # Frame principal
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(pady=20)
 
-# Configuração para layout responsivo
-janela.grid_rowconfigure(0, weight=1)
-janela.grid_columnconfigure(0, weight=1)
+        # Elementos da aba Mensagens
+        self.create_mensagens_tab()
 
-# Rótulo para instrução
-rotulo_instrucao = tk.Label(janela, text="Selecione uma opção:", font=("Arial", 14), bg='#dcdde1')
-rotulo_instrucao.grid(row=1, column=0, padx=20, pady=0, sticky="w")
+    def center_window(self):
+        # Obtém as dimensões da tela
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
 
-# Caixa de combinação
-opcoes = ["Mensagem de Texto", "Mensagem de Imagem"]
-combobox = ttk.Combobox(janela, values=opcoes, state="readonly", font=("Arial", 12))  # "readonly" impede edição
-combobox.grid(row=2, column=0, padx=(20, 300), pady=(0,5), sticky="w")
-combobox.set("Escolha uma opção")  # Define um texto inicial
+        # Calcula a posição x e y para centralizar a janela
+        x = (screen_width // 2) - (450 // 2)
+        y = (screen_height // 2) - (450 // 2)
 
-# Rótulo e botão para selecionar arquivo de contatos
-rotulo_contatos = tk.Label(janela, text="Lista de Contatos (.txt):", font=("Arial", 14), bg='#dcdde1')
-rotulo_contatos.grid(row=3, column=0, padx=20, pady=(10, 0), sticky="w")
-entrada_arquivo = tk.Entry(janela, font=("Arial", 12), width=50)
-entrada_arquivo.grid(row=4, column=0, padx=(20, 60), pady=(0, 5), sticky="w")
-botao_arquivo = tk.Button(janela, text="Anexar", command=selecionar_arquivo)
-botao_arquivo.grid(row=5, column=0, ipadx=7,ipady=3,padx=20, pady=(5, 10), sticky="w")
+        # Define a posição da janela
+        self.root.geometry(f"450x450+{x}+{y}")
 
-# Rótulo e caixa de texto para a mensagem
-rotulo_mensagem = tk.Label(janela, text="Mensagem:", font=("Arial", 14), bg='#dcdde1')
-rotulo_mensagem.grid(row=6, column=0, padx=20, pady=(10, 0), sticky="w")
-caixa_texto = tk.Text(janela, height=6, font=("Arial", 12), wrap="word", width=30)  # Ajusta a lwhatsappargura
-caixa_texto.grid(row=7, column=0, padx=20, pady=(5, 10), sticky="ew")
+    def create_mensagens_tab(self):
+        self.label_arquivo = tk.Label(self.frame, text="Anexar arquivo .txt com contatos:")
+        self.label_arquivo.pack()
 
-# Botão para confirmar a seleção
-botao = tk.Button(janela, text="Executar")
-botao.grid(row=8, column=0, ipadx=7,ipady=3,padx=10, pady=(15,20), sticky="")
+        self.txt_arquivo = tk.Text(self.frame, height=1, width=40)
+        self.txt_arquivo.pack(pady=5)
 
-# Configurar o botão para executar a função com base na seleção
-botao.config(command=lambda: executar(combobox))
+        self.btn_anexar = tk.Button(self.frame, text="Escolher arquivo", command=self.load_file)
+        self.btn_anexar.pack()
 
-# Expansão dos elementos (responsividade)
-janela.grid_columnconfigure(0, weight=1)
+        self.label_tipo_mensagem = tk.Label(self.frame, text="Escolha o tipo da mensagem:")
+        self.label_tipo_mensagem.pack(pady=(20,0))
 
-# Inicia o loop principal
-janela.mainloop()
+        self.var_tipo = tk.StringVar(value="texto")
+        self.radio_texto = tk.Radiobutton(self.frame, text="Texto", variable=self.var_tipo, value="texto", command=self.update_message_input)
+        self.radio_imagem = tk.Radiobutton(self.frame, text="Imagem", variable=self.var_tipo, value="imagem", command=self.update_message_input)
+        self.radio_texto.pack()
+        self.radio_imagem.pack()
+
+        self.input_mensagem = None
+        self.txt_imagem = None
+        self.input_imagem = None
+        self.btn_enviar = None  # Inicializa o botão de enviar como None
+
+        self.update_message_input()  # Atualiza a interface com base na seleção inicial
+
+    def load_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+        if file_path:
+            messagebox.showinfo("Arquivo Selecionado", f"Arquivo selecionado: {file_path}")
+
+    def update_message_input(self):
+         # Remove os campos anteriores, se existirem
+        if self.input_mensagem:
+            self.input_mensagem.destroy()
+        if self.input_imagem:
+            self.input_imagem.destroy()
+        if self.btn_enviar:  # Destrói o botão de enviar se existir
+            self.btn_enviar.destroy()
+        if self.txt_imagem:
+            self.txt_imagem.destroy()
+
+        # Adiciona novo campo baseado na seleção
+        if self.var_tipo.get() == "texto":
+            self.input_mensagem = tk.Text(self.frame, height=10, width=40)
+            self.input_mensagem.pack(pady=10)
+            self.input_mensagem.insert(tk.INSERT, "Digite o modelo da mensagem")
+            
+        else:
+            self.txt_imagem = tk.Text(self.frame, height=1, width=40)
+            self.txt_imagem.pack()
+            self.input_imagem = tk.Button(self.frame, text="Escolher imagem", command=self.load_image)
+            self.input_imagem.pack(pady=10)
+
+        # Cria o botão de enviar
+        self.btn_enviar = tk.Button(self.frame, text="Iniciar Envio", command=self.send_message)
+        self.btn_enviar.pack(pady=10)
+
+
+    def load_image(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+        if file_path:
+            messagebox.showinfo("Imagem Selecionada", f"Imagem selecionada: {file_path}")
+
+    def send_message(self):
+        if self.var_tipo.get() == "texto":
+            message = self.input_mensagem.get("1.0", "end-1c")
+            messagebox.showinfo("Mensagem Enviada", f"Mensagem enviada: {message}")
+        else:
+            messagebox.showinfo("Mensagem Enviada", "Imagem enviada com sucesso!")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
